788d924035b46b8446401d2ad2598f67dddeb4d4
SushantShelake <124454809+sushant2209@users.noreply.github.com>
2024-09-01 19:00:46 +0530

    Update sendToTelegram.py

--- telegram_app/sendToTelegram.py
+++ telegram_app/sendToTelegram.py
@@ -2,16 +2,18 @@ from telethon import TelegramClient
 from .xtelethon import CustomParseMode  # Import your custom class
 from django.conf import settings
 from datetime import timedelta
+from telethon.sessions import StringSession
 import os
 
 # Define the path for the GIF if needed
 gif_path = os.path.join(settings.MEDIA_ROOT, "SampleGIF.gif")
 
 async def send_message_to_telegram(message):
+    client = TelegramClient(StringSession(settings.SESSION_STRING), settings.API_ID, settings.API_HASH)
+
     api_id = settings.TELEGRAM_API_ID
     api_hash = settings.TELEGRAM_API_HASH
-    session = 'session'
-    client = TelegramClient(session, api_id, api_hash)
+    client = TelegramClient('session', api_id, api_hash)
 
     await client.start()  # Start the client asynchronously
     client.parse_mode = CustomParseMode('markdown')  # Set parse_mode if needed
